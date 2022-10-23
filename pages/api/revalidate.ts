932038179d62f084cecbe16ac1fb3c1750f7882d
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-10-23 11:31:14 +0000

    FIx: unstable_revalidateをrevalidateに変更

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -2,8 +2,9 @@ export default async function handler(req, res) {
   if (req.query.secret !== process.env.SUPABASE_SERVICE_ROLE_KEY as string) {
     return res.status(401).json({ message: 'Invalid token' })
   }
+
   try {
-    await res.unstable_revalidate('/')
+    await res.revalidate('/')
     return res.json({ revalidated: true })
   } catch (err) {
     return res.status(500).send(`${err}\nError revalidating`)
