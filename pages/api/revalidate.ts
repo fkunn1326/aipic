551229f81cf52432ecb9f92b71aada2c474c2a15
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-10-23 10:59:18 +0000

    Add: ISRへの対応を追加

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    if (req.query.secret !== process.env.REVALIDATE_SECRET) {
+    if (req.query.secret !== process.env.SUPABASE_SERVICE_ROLE_KEY) {
       return res.status(401).json({ message: 'Invalid token' });
     }
     try {
@@ -8,5 +8,4 @@ export default async function handler(req, res) {
     } catch (err) {
       return res.status(500).send('Error revalidating');
     }
-  }
-  
+}  
