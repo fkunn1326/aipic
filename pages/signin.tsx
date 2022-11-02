80849fc218ff5d427959d48290be1610732be1d8
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-11-02 03:49:09 +0000

    Add: 閲覧数、いいね数、コピー数の表示

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -44,12 +44,14 @@ const Signin = () => {
   return (
     <div className="bg-gray-50 pt-10">
       <div className="flex flex-col items-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a
-          href="#"
-          className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
-        >
-          AI Arts
-        </a>
+        <Link href="/">
+          <a
+            href="#"
+            className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
+          >
+            AI Arts
+          </a>
+        </Link>
         <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -49,12 +49,13 @@ const Signup = () => {
   return (
     <div className="bg-gray-50 pt-10 mb-10">
       <div className="flex flex-col items-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a
-          href="#"
-          className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
-        >
-          AI Arts
-        </a>
+        <Link href="/">
+          <a
+            className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
+          >
+            AI Arts
+          </a>
+        </Link>
         <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           {!ismailsent ? (
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
