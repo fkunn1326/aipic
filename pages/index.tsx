11ac3b52b4464b133b28ff19f4a52c77b0fc8921
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-11-03 04:40:26 +0000

    Fix:スマホでのCSSの修正＆コードのリファクタ＆…ボタンの修正&タグの余白(#1 #8 #12)

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,8 @@ import React, { useState, useEffect, useContext } from "react";
 import Header from "../components/header/header";
 import { userInfoContext } from "../context/userInfoContext";
 import useSWR from "swr";
-import Link from "next/link";
-
-function cn(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
+import BlurImage from "../components/common/BlurImage"
+import SkeletonImage from "../components/common/SkeltonImage"
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
@@ -50,82 +47,6 @@ export default function App() {
             <BlurImage key={image.id} image={image} />
           ))}
         </div>
-      </div>
-    </div>
-  );
-}
-
-function BlurImage({ image }) {
-  const [isLoading, setLoading] = useState(true);
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  return (
-    <div className="group" onClick={() => setIsOpen(true)}>
-      <div className="relative">
-        <div className="cursor-pointer aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200">
-          <Link href={`/images/${image.id}`}>
-            <Image
-              alt={image.title}
-              src={image.href}
-              layout="fill"
-              objectFit="cover"
-              className={cn(
-                "duration-700 ease-in-out group-hover:opacity-75",
-                isLoading
-                  ? "scale-110 blur-2xl grayscale"
-                  : "scale-100 blur-0 grayscale-0"
-                // image.age_limit !== 'all'
-                //   ? 'blur-md'
-                //   : ''
-              )}
-              onLoadingComplete={() => {
-                setLoading(false);
-              }}
-            />
-          </Link>
-        </div>
-        {image.age_limit === "nsfw" && (
-          <p className="absolute bottom-2 right-4 text-sm font-semibold bg-red-500 text-white px-2 rounded-md">
-            NSFW
-          </p>
-        )}
-        {image.age_limit === "r18" && (
-          <p className="absolute bottom-2 right-4 text-sm font-semibold bg-red-500 text-white px-2 rounded-md">
-            R-18
-          </p>
-        )}
-        {image.age_limit === "r18g" && (
-          <p className="absolute bottom-2 right-4 text-sm font-semibold bg-red-500 text-white px-2 rounded-md">
-            R-18G
-          </p>
-        )}
-      </div>
-      <p className="mt-2 text-base font-semibold text-gray-900 text-ellipsis whitespace-nowrap overflow-hidden">
-        {image.title}
-      </p>
-      <Link href={`/users/${image.author.uid}`}>
-        <a className="mt-1 w-full flex items-center">
-          <Image
-            src={image.author.avatar_url}
-            width={20}
-            height={20}
-            className="rounded-full"
-          ></Image>
-          <h3 className="ml-2 text-base text-gray-700">{image.author.name}</h3>
-        </a>
-      </Link>
-    </div>
-  );
-}
-
-function SkeletonImage() {
-  return (
-    <div className="group">
-      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 animate-pulse"></div>
-      <div className="mt-2 rounded-full bg-gray-200 h-3 w-32"></div>
-      <div className="mt-1 w-full flex items-center">
-        <div className="h-5 w-5 rounded-full bg-gray-200"></div>
-        <div className="ml-2 rounded-full bg-gray-200 h-3 w-16"></div>
       </div>
     </div>
   );
