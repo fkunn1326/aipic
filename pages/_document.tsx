f1d64472d144a4be8d151468d0ebfa31a0df16b3
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-11-03 06:57:14 +0000

    Add: PWA実装 (#16)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,11 @@ import React from "react";
 const Document = () => {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="#fff" />
+      </Head> 
       <body>
         <Main />
         <NextScript />
