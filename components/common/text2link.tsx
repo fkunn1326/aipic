86976ad73ecedeab9309001aa0b4d54aea656601
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-11-03 09:23:52 +0000

    Fix: XSSできないように修正

--- components/common/text2link.tsx
+++ components/common/text2link.tsx
@@ -1,6 +1,9 @@
+import DOMPurify from 'isomorphic-dompurify';
+
 export const text2Link = (text: string) => {
+    let cleantext = DOMPurify.sanitize(text);
     const regexp_url = /(https?:\/\/[\w/:%#\$&\?\(\)~\.=\+\-]+)/g;
-    let linkedComment = text.replace(regexp_url, '<a href="$1" class="text-sky-600" target="_blank" rel="noopener noreferrer">$1</a>');
+    let linkedComment = cleantext.replace(regexp_url, '<a href="$1" class="text-sky-600" target="_blank" rel="noopener noreferrer">$1</a>');
   
     return linkedComment;
 };
