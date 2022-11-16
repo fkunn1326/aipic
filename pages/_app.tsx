0c330d77aa772c535ba1b7c6f308af04556dee92
fkunn1326@users.noreply.github.com <fkunn1326>
2022-11-16 03:02:20 +0000

    Add: init analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,12 +7,16 @@ import UserInfoProvider from "../components/auth/userInfoProvider";
 import useTransition from "../components/hooks/useTransition";
 import { SiteName } from "../components/core/const"
 import { appWithTranslation } from 'next-i18next';
+import { GATracking } from "../components/GaTracking" 
+
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID as string
 
 function MyApp({ Component, pageProps }: AppProps) {
   useTransition();
   return (
     <UserProvider supabaseClient={supabaseClient}>
       <UserInfoProvider>
+        <GATracking trackingId={GA_TRACKING_ID} />
         <Component {...pageProps} />
       </UserInfoProvider>
     </UserProvider>
