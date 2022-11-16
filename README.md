9ad867a0e1204e6ac14a60409ed3b8d4d07e315c
fkunn1326@users.noreply.github.com <fkunn1326>
2022-11-16 16:31:14 +0000

    Add: newページにおけるページネーションの実装

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# AIPIC（あいぴく）
+
+[AIPIC](https://www.aipic.app) は、[supabase](https://supabase.com/)と、[cloudflareR2]
 
 ## Getting Started
 
--- pages/new.tsx
+++ pages/new.tsx
@@ -9,11 +9,15 @@ import SkeletonImage from "../components/common/SkeltonImage"
 import { ArrowUpIcon } from "@heroicons/react/24/solid";
 import { supabaseClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
 export default function App() {
   var ctx = useContext(userInfoContext);
+  const router = useRouter()
+  const page = router.query.page !== undefined ? parseInt(router.query.page as string) : 1
+
   var access_limit = ""
 
   if (ctx.UserInfo !== null) {
@@ -21,12 +25,32 @@ export default function App() {
   }
 
   const { data, error } = useSWR(
-    "../api/images/list" + access_limit,
+    "../api/images/list" + access_limit + "&page=" + page,
      fetcher,
-     {
-        fallbackData: []
-     }
   );
+
+
+  const getpagenation = () => {
+    var startPage = page - 2;
+    var endPage = page + 2;
+    var arr: any[] = []
+
+    if (startPage <= 0) {
+        endPage -= (startPage - 1);
+        startPage = 1;
+    }
+
+    if (endPage > totalPage)
+        endPage = totalPage;
+
+    if (startPage > 1) arr.push("...");
+    for(var i=startPage; i<=endPage; i++) arr.push(i);
+    if (endPage < totalPage) arr.push("...");
+    if (endPage < totalPage) arr.push(totalPage)
+    if (startPage > 1 && endPage < totalPage) arr.splice(0,1)
+    
+    return arr
+  }
 
   if (!data)
     return (
@@ -34,7 +58,7 @@ export default function App() {
         <Header></Header>
         <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
           <div className="grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 xl:gap-x-8">
-            {Array.apply(0, Array(10)).map(function (x, i) {
+            {Array.apply(0, Array(20)).map(function (x, i) {
               return <SkeletonImage key={i} />;
             })}
           </div>
@@ -42,6 +66,8 @@ export default function App() {
         <Footer/>
       </div>
   );
+
+  const totalPage = Math.ceil(data.count / 20)
 
   return (
     <div className="dark:bg-slate-900">
@@ -55,7 +81,7 @@ export default function App() {
       </div>
       <div className="mx-auto max-w-2xl mb-12 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 xl:gap-x-8">
-          {data.map((image) => (
+          {data.body.map((image) => (
             <BlurImage key={image.id} image={image} />
           ))}
         </div>
@@ -63,6 +89,15 @@ export default function App() {
       <button className="fixed right-0 bottom-0" onClick={() => {window.scrollTo({top: 0, behavior: 'smooth'})}}>
         <ArrowUpIcon className="w-12 h-12 bg-gray-400 text-white rounded-full p-3 m-12" />
       </button>
+      <div className="w-full flex gap-x-4 justify-center">
+      </div>
+      <div className="flex justify-center gap-x-4 text-lg">
+        {getpagenation().map((count,idx) => (
+          <Link href={`${count !== "..." ? `/new?page=${count}` : `/new?page=${page}`}`} key={idx}>
+            <button key={idx} className={`${page === count && "font-semibold text-sky-600"}`} >{count}</button>
+          </Link>
+        ))}
+      </div>
       <Footer/>
     </div>
   );
