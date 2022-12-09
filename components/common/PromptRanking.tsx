28fa569556e320849453f5bd72abcb16fc269237
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-12-09 17:15:30 +0900

    Fix: レイアウト変更

--- components/common/PromptRanking.tsx
+++ components/common/PromptRanking.tsx
@@ -36,11 +36,11 @@ export default function PromptRanking() {
   return (
     <div>
       <div className="mx-auto max-w-2xl mb-12 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="grid grid-cols-2 gap-y-10 gap-x-6 sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 xl:gap-x-8">
+        <div className="grid grid-cols-2 gap-y-10 sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-x-4 xl:gap-x-6">
           {images.map((image, idx) => (
             <div className="relative">
             	<BlurImage key={image.id} image={image} pr preview />
-							<p className="absolute bottom-0 left-0 px-4 pb-4 pt-32 w-full rounded-b-xl text-white font-semibold bg-gradient-to-t from-transparent/[0.3] to-transparent">{image.prompt}</p>
+							<p className="absolute bottom-0 left-0 px-4 pb-3.5 pt-32 w-full rounded-b-xl text-white font-semibold bg-gradient-to-t from-transparent/[0.3] to-transparent">{image.prompt}</p>
             </div>
           ))}
         </div>
